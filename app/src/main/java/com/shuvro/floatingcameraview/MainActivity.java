bd75c8cef104e5b90a86d25c475408ae3fa3c132
Shuvro Pal <shuvropal@gmail.com>
2020-09-18 03:50:33 +0600

    Initial commit

---
+++ app/src/main/java/com/shuvro/floatingcameraview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.shuvro.floatingcameraview;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_DRAW_OVERLAY_PERMISSION = 5;
+    private CameraFloatingWindow cameraFloatingWindow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraFloatingWindow = new CameraFloatingWindow(getApplicationContext());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_DRAW_OVERLAY_PERMISSION) {
+
+            if (Settings.canDrawOverlays(this)) {
+                cameraFloatingWindow.show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Permission not granted.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void startManageDrawOverlaysPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, REQUEST_CODE_DRAW_OVERLAY_PERMISSION);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void btn_click(View view) {
+        if(Settings.canDrawOverlays(getApplicationContext())){
+            cameraFloatingWindow.show();
+        }else{
+            startManageDrawOverlaysPermission();
+        }
+    }
+}
